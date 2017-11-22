ca6216a33f425f2ae2ad843c586c9e627d7dbf39
Scheepers <Extremelyd1@outlook.com>
2017-11-22 16:30:45 +0100

    Added extra classes

---
+++ src/main/java/Graph.java
@@ -0,0 +1,15 @@
+public class Graph {
+
+    public void addVertex() {
+
+    }
+
+    public void addVertices(int n) {
+
+    }
+
+    public void addEdge(int a, int b) {
+
+    }
+
+}
---
+++ src/main/java/Vertex.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Vertex {
+
+    ArrayList<Vertex> connection;
+
+    public Vertex() {
+
+        connection = new ArrayList<Vertex>();
+
+    }
+
+}
